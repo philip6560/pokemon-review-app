1d1dca5e44663b7611fdfd39c3b8064f6decd2a5
Philip Okhae <okhaephilip2@gmail.com>
2024-08-13 22:23:22 +0100

    [chore]: project base setup is done, dbContext and Entities setup is done

---
+++ PokemonReviewApp/Models/Owner.cs
@@ -0,0 +1,12 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Gym { get; set; }
+
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+    }
+}
---
+++ PokemonReviewApp/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Review
+    {
+        public int Id { get; set; } 
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+    }
+}
